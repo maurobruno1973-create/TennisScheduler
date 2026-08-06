809648e9ae69d2cf0e253da829862203a052b4b8
maurobruno1973-create <maurobruno1973@gmail.com>
2026-08-06 11:37:38 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,34 @@
+"""
+Configurazione del torneo
+Modifica solo questo file se cambiano coppie o parametri.
+"""
+
+# Numero di partite da selezionare
+NUM_MATCHES = 18
+
+# Coppie disponibili (Uomo, Donna)
+PAIRS = [
+    ("Ash", "Tiantian"),
+    ("Ash", "Lina"),
+
+    ("Graeme", "Beth"),
+    ("Graeme", "Matilde"),
+    ("Graeme", "Maryna"),
+
+    ("Quentin", "Tiantian"),
+    ("Quentin", "Maryna"),
+    ("Quentin", "Matilde"),
+
+    ("Mauro", "Beth"),
+    ("Mauro", "Paola"),
+    ("Mauro", "Lina"),
+]
+
+# Vincoli principali
+TARGET_MATCHES_PER_WOMAN = 6
+
+# Preferenze (non vincoli rigidi)
+AVOID_GRAEME_VS_PAOLA = True
+MAXIMIZE_DIFFERENT_OPPONENTS = True
+MINIMIZE_REMATCHES = True
+BALANCE_MEN = True

90fb68ecfd7fe1cdd0cb3416aa8d8567e7a49516
maurobruno1973-create <maurobruno1973@gmail.com>
2026-08-06 11:38:15 +0100

    Create scheduler.py

---
+++ scheduler.py
@@ -0,0 +1,12 @@
+"""
+Tennis Mixed Doubles Scheduler
+
+Versione: 0.1
+Autore: Mauro Bruno + ChatGPT
+
+Questo file conterrà il motore del programma.
+"""
+
+from config import *
+
+print("Scheduler caricato correttamente")

e4a6ce3cfcec23a64471ff5edf050c46719005ec
maurobruno1973-create <maurobruno1973@gmail.com>
2026-08-07 17:18:50 +0100

    main.py V0.14
    Add check_pair_distribution and build_women_deviation_variables

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from excel_reporter import create_excel_report
 if __name__ == "__main__":
 
     scheduler = TournamentScheduler()
-
     scheduler.generate_matches()
+    scheduler.check_pair_distribution()
+    
     scheduler.build_model()
     scheduler.build_player_counters()
     scheduler.build_player_count_variables()
     scheduler.compute_targets()
 
+    scheduler.build_women_deviation_variables()
     scheduler.build_men_deviation_variables()
     scheduler.build_soft_avoid_variables()
     scheduler.build_opponent_count_variables()
